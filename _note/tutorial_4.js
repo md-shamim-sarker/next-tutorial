3624b38ca6e3601640b63795396b4c1ed2793cff
shamimspro <shamimspro@gmail.com>
2023-01-23 23:17:06 +0600

    next.js routing

---
+++ _note/tutorial_4.js
@@ -0,0 +1,21 @@
+/*
+Routing Section Intro:
+
+Routing in a React app
+* Install a third party package
+* routes.js file to configure the routes
+* For each route, create a component file, export the component, import it in routes.js and configure the new route with a path property
+
+Routing in a Next.js app
+*  File-system based routing mechanism
+*  When a file is added to the pages folder in a project, it automatically becomes available as a route
+* By mixing and matching file names with a nested folder structure, it is possible to pretty much define the most common routing patterns
+
+Routing in a Next.js app
+* Route with pages
+* Nested routes
+* Dynamic routes
+* Catch-all routes
+* Navigate from the UI
+* Programmatically navigate b/w pages
+*/
