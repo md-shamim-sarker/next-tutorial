933f618206d6de1226552ea2c1dc8d3192d0de6c
shamimspro <shamimspro@gmail.com>
2023-01-23 18:09:17 +0600

    basic discussion of next.js

---
+++ _note/tutorial_1.js
@@ -0,0 +1,36 @@
+/*
+What is Next.js?
+----------------
+The React Framework for Production.
+
+React:
+------
+Not quite possible to build a full feature rich application ready to be deployed for production
+React is a library for building user interfaces
+You have to make decisions on other features of the app like routing, styling, authentication etc.
+
+Next.js
+-------
+A package that uses React for building user interfaces
+Loaded with a lot more features that enable you to build full fledged production ready applications. Features exactly like routing, styling, authentication, bundle optimization etc.
+
+There's no need to install additional packages. Next.js provides everything for you.
+
+Opinion and conventions need to be followed to implement the above said features.
+
+Why learn Next.js?
+------------------
+Next. simplifies the process of building a react application for production
+1. File based routing
+2. Pre-rendering
+3. API routes
+4. Support for CSS modules
+5. Authentication
+6. Dev and Prod build system
+
+Pre-requisites:
+---------------
+HTML, CSS and JavaScript Fundamentals
+ES6 + features
+React Fundamentals
+*/
