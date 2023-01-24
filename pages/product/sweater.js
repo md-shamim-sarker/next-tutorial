8e916841dbd731d55ddd5985db1a1ee0336cb456
shamimspro <shamimspro@gmail.com>
2023-01-24 09:39:02 +0600

    dynamic routes

---
+++ pages/product/sweater.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sweater = () => {
+    return (
+        <div>
+            <h2>This is sweater landing page.</h2>
+        </div>
+    );
+};
+
+export default Sweater;
